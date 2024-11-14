1eecad547399b77ce8f35e699aa427d1f626ab7d
bhn9 <bhn9@cornell.edu>
2024-11-14 16:47:30 -0500

    Glide changes

--- Assets/Bird/Bird Controller.cs
+++ Assets/Bird/Bird Controller.cs
@@ -14,7 +14,7 @@ public class BirdController : MonoBehaviour
 
     [SerializeField] private float flapTerminalX = 5f;
     [SerializeField] private float flapTerminalY = 8f;
-    [SerializeField] private float glideTerminalX = 8f;
+    [SerializeField] private float glideTerminalX = 20f;
     [SerializeField] private float gildeTerminalY = -1f;
     [SerializeField] private float diveTerminalX = 2f;
     [SerializeField] private float diveTerminalY = -10f;
@@ -23,6 +23,7 @@ public class BirdController : MonoBehaviour
     private Animator animator;
     private Rigidbody rb;
     private PlayerInput playerInput;
+    private Transform eagleCenter;
 
     private BirdState state;
 
@@ -33,6 +34,7 @@ public class BirdController : MonoBehaviour
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
+        eagleCenter = GameObject.Find("Eagle_Normal").transform;
 
         state = BirdState.Ground;
     }
@@ -82,12 +84,11 @@ public class BirdController : MonoBehaviour
     void GetFlapDirection()
     {
         Vector2 horizontalInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector3 facing = new Vector3(horizontalInput.x, 0, horizontalInput.y);
+        Vector3 horizontalInput3D = new Vector3(horizontalInput.x, 0, horizontalInput.y);
 
-        if (facing != Vector3.zero)
+        if (horizontalInput3D != Vector3.zero)
         {
-            //transform.forward = facing;
-            rb.AddForce(facing * flapForce, ForceMode.Acceleration);
+            rb.AddForce(horizontalInput3D * flapForce, ForceMode.Acceleration);
         }
         else
         {
@@ -95,6 +96,7 @@ public class BirdController : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x * damp, rb.velocity.y, rb.velocity.z * damp);
         }
 
+        //transform.rotation = Quaternion.Euler(0, 0, horizontalInput3D.x * -20);
 
         Vector2 horizontalVelocity = new(rb.velocity.x, rb.velocity.z);
         if (horizontalVelocity.magnitude > flapTerminalX)
@@ -102,82 +104,37 @@ public class BirdController : MonoBehaviour
             horizontalVelocity = horizontalVelocity.normalized * flapTerminalX;
             rb.velocity = new Vector3(horizontalVelocity.x, rb.velocity.y, horizontalVelocity.y);
         }
-
-        // if (playerInput.actions["Forward"].IsPressed())
-        // {
-        //     if (rb.velocity.z < flapTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.forward * flapForce, ForceMode.Acceleration);
-        //         if (rb.velocity.z > flapTerminalX) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, flapTerminalX);
-        //     }
-        // }
-        // else if (playerInput.actions["Back"].IsPressed())
-        // {
-        //     if (rb.velocity.z > -flapTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.back * flapForce, ForceMode.Acceleration);
-        //         if (rb.velocity.z < -flapTerminalX) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -flapTerminalX);
-        //     }
-        // }
-
-        // if (playerInput.actions["Left"].IsPressed())
-        // {
-        //     if (rb.velocity.x > -flapTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.left * flapForce, ForceMode.Acceleration);
-        //         if (rb.velocity.x < -flapTerminalY) rb.velocity = new Vector3(-flapTerminalY, rb.velocity.y, rb.velocity.z);
-        //     }
-        //     //add angle left
-        // }
-        // else if (playerInput.actions["Right"].IsPressed())
-        // {
-        //     if (rb.velocity.x < flapTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.right * flapForce, ForceMode.Acceleration);
-        //         if (rb.velocity.x > flapTerminalY) rb.velocity = new Vector3(flapTerminalY, rb.velocity.y, rb.velocity.z);
-        //     }
-        //     //add angle right
-        // }
     }
 
     void GetGlideDirection()
     {
-        // if (playerInput.actions["Forward"].IsPressed())
-        // {
+        float glideScale = 1;
+        Vector2 horizontalInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 horizontalInput3D = new Vector3(horizontalInput.x, 0, horizontalInput.y);
 
-        // }
-        // else if (playerInput.actions["Back"].IsPressed())
-        // {
+        if (horizontalInput.y > 0)
+        {
+            glideScale = 1.5f;
+        }
+        else if (horizontalInput.y < 0)
+        {
+            glideScale = .5f;
+        }
 
-        // }
-        // else
-        // {
-        //     if (rb.velocity.z < glideTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.forward * 3, ForceMode.Acceleration);
-        //         if (rb.velocity.z > glideTerminalX) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, glideTerminalX);
-        //     }
-        // }
+        if (Math.Abs(transform.rotation.eulerAngles.z) <= 20)
+        {
+            //transform.RotateAround(eagleCenter.position, Vector3.forward, -Math.Sign(horizontalInput.x));
+        }
 
+        rb.AddForce(Vector3.forward * 5 * glideScale, ForceMode.Acceleration);
 
-        // if (playerInput.actions["Left"].IsPressed())
-        // {
-        //     if (rb.velocity.x > -flapTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.left * flapForce, ForceMode.Acceleration);
-        //         if (rb.velocity.x < -flapTerminalY) rb.velocity = new Vector3(-flapTerminalY, rb.velocity.y, rb.velocity.z);
-        //     }
-        //     //add angle left
-        // }
-        // else if (playerInput.actions["Right"].IsPressed())
-        // {
-        //     if (rb.velocity.x < flapTerminalX)
-        //     {
-        //         rb.AddForce(Vector3.right * flapForce, ForceMode.Acceleration);
-        //         if (rb.velocity.x > flapTerminalY) rb.velocity = new Vector3(flapTerminalY, rb.velocity.y, rb.velocity.z);
-        //     }
-        //     //add angle right
-        // }
+        Vector2 horizontalVelocity = new(rb.velocity.x, rb.velocity.z);
+        if (horizontalVelocity.magnitude > glideTerminalX)
+        {
+            horizontalVelocity = horizontalVelocity.normalized * glideTerminalX;
+            rb.velocity = new Vector3(horizontalVelocity.x, rb.velocity.y, horizontalVelocity.y);
+        }
+
     }
 
 
