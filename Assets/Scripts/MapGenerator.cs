9c9685e7fd0ee9469fe215f334ff6d001f8bad1d
al967 <al967@cornell.edu>
2024-11-13 23:14:35 -0500

    vegetation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,20 +4,22 @@ using System;
 using System.Threading;
 using System.Collections.Generic;
 
-public class MapGenerator : MonoBehaviour {
 
-	public enum DrawMode {NoiseMap, ColourMap, Mesh, FalloffMap};
+public class MapGenerator : MonoBehaviour
+{
+
+	public enum DrawMode { NoiseMap, ColourMap, Mesh, FalloffMap };
 	public DrawMode drawMode;
 
 	public Noise.NormalizeMode normalizeMode;
 
 	public const int mapChunkSize = 241;
-	[Range(0,6)]
+	[Range(0, 6)]
 	public int editorPreviewLOD;
 	public float noiseScale;
 
 	public int octaves;
-	[Range(0,1)]
+	[Range(0, 1)]
 	public float persistance;
 	public float lacunarity;
 
@@ -33,116 +35,186 @@ public class MapGenerator : MonoBehaviour {
 
 	public TerrainType[] regions;
 
+	public VegetationRule[] vegetationRules;
+	public bool autoUpdateVegetation = true;
+	private Transform vegetationParent;
+	public float placementThreshold = 0.6f;
+
+	public MapData md;
+
 	float[,] falloffMap;
 
 	Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
 	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
+	Queue<VegetationPlacementRequest> vegetationPlacementQueue = new Queue<VegetationPlacementRequest>();
 
-	void Awake() {
-		falloffMap = FalloffGenerator.GenerateFalloffMap (mapChunkSize);
+	void Awake()
+	{
+		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
 	}
 
-	public void DrawMapInEditor() {
-		MapData mapData = GenerateMapData (Vector2.zero);
+	public void DrawMapInEditor()
+	{
+		MapData mapData = GenerateMapData(Vector2.zero);
+		this.md = mapData;
+		Debug.Log(this.md == mapData);
 
-		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		if (drawMode == DrawMode.NoiseMap) {
-			display.DrawTexture (TextureGenerator.TextureFromHeightMap (mapData.heightMap));
-		} else if (drawMode == DrawMode.ColourMap) {
-			display.DrawTexture (TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
-		} else if (drawMode == DrawMode.Mesh) {
-			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
-		} else if (drawMode == DrawMode.FalloffMap) {
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+		if (drawMode == DrawMode.NoiseMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.heightMap));
+		}
+		else if (drawMode == DrawMode.ColourMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(mapData.colourMap, mapChunkSize, mapChunkSize));
+		}
+		else if (drawMode == DrawMode.Mesh)
+		{
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD),
+					TextureGenerator.TextureFromColourMap(mapData.colourMap, mapChunkSize, mapChunkSize));
+
+			// Place vegetation in editor
+			if (autoUpdateVegetation)
+			{
+				PlaceVegetation(mapData, Vector2.zero);
+			}
+		}
+		else if (drawMode == DrawMode.FalloffMap)
+		{
 			display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapChunkSize)));
 		}
 	}
 
-	public void RequestMapData(Vector2 centre, Action<MapData> callback) {
-		ThreadStart threadStart = delegate {
-			MapDataThread (centre, callback);
+	public void RequestMapData(Vector2 centre, Action<MapData> callback)
+	{
+		ThreadStart threadStart = delegate
+		{
+			MapDataThread(centre, callback);
 		};
 
-		new Thread (threadStart).Start ();
+		new Thread(threadStart).Start();
 	}
 
-	void MapDataThread(Vector2 centre, Action<MapData> callback) {
-		MapData mapData = GenerateMapData (centre);
-		lock (mapDataThreadInfoQueue) {
-			mapDataThreadInfoQueue.Enqueue (new MapThreadInfo<MapData> (callback, mapData));
-		}
-	}
-
-	public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback) {
-		ThreadStart threadStart = delegate {
-			MeshDataThread (mapData, lod, callback);
-		};
-
-		new Thread (threadStart).Start ();
-	}
-
-	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback) {
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
-		lock (meshDataThreadInfoQueue) {
-			meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData> (callback, meshData));
-		}
-	}
-
-	void Update() {
-		if (mapDataThreadInfoQueue.Count > 0) {
-			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++) {
-				MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue ();
-				threadInfo.callback (threadInfo.parameter);
-			}
-		}
-
-		if (meshDataThreadInfoQueue.Count > 0) {
-			for (int i = 0; i < meshDataThreadInfoQueue.Count; i++) {
-				MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue ();
-				threadInfo.callback (threadInfo.parameter);
+	void MapDataThread(Vector2 centre, Action<MapData> callback)
+	{
+		MapData mapData = GenerateMapData(centre);
+		lock (mapDataThreadInfoQueue)
+		{
+			mapDataThreadInfoQueue.Enqueue(new MapThreadInfo<MapData>(callback, mapData));
+			if (autoUpdateVegetation)
+			{
+				lock (vegetationPlacementQueue)
+				{
+					vegetationPlacementQueue.Enqueue(new VegetationPlacementRequest(mapData, centre));
+				}
 			}
 		}
 	}
 
-	MapData GenerateMapData(Vector2 centre) {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, centre + offset, normalizeMode);
+	public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback)
+	{
+		ThreadStart threadStart = delegate
+		{
+			MeshDataThread(mapData, lod, callback);
+		};
+
+		new Thread(threadStart).Start();
+	}
+
+	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback)
+	{
+		MeshData meshData = MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
+		lock (meshDataThreadInfoQueue)
+		{
+			meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
+		}
+	}
+
+	void Update()
+	{
+		if (mapDataThreadInfoQueue.Count > 0)
+		{
+			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++)
+			{
+				MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue();
+				threadInfo.callback(threadInfo.parameter);
+			}
+		}
+
+		if (meshDataThreadInfoQueue.Count > 0)
+		{
+			for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
+			{
+				MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
+				threadInfo.callback(threadInfo.parameter);
+			}
+		}
+
+		// Process vegetation placement requests
+		if (vegetationPlacementQueue.Count > 0)
+		{
+			lock (vegetationPlacementQueue)
+			{
+				while (vegetationPlacementQueue.Count > 0)
+				{
+					VegetationPlacementRequest request = vegetationPlacementQueue.Dequeue();
+					PlaceVegetation(this.md, request.centre);
+				}
+			}
+		}
+	}
+
+	MapData GenerateMapData(Vector2 centre)
+	{
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, centre + offset, normalizeMode);
 
 		Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
-		for (int y = 0; y < mapChunkSize; y++) {
-			for (int x = 0; x < mapChunkSize; x++) {
-				if (useFalloff) {
-					noiseMap [x, y] = Mathf.Clamp01(noiseMap [x, y] - falloffMap [x, y]);
+		for (int y = 0; y < mapChunkSize; y++)
+		{
+			for (int x = 0; x < mapChunkSize; x++)
+			{
+				if (useFalloff)
+				{
+					noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - falloffMap[x, y]);
 				}
-				float currentHeight = noiseMap [x, y];
-				for (int i = 0; i < regions.Length; i++) {
-					if (currentHeight >= regions [i].height) {
-						colourMap [y * mapChunkSize + x] = regions [i].colour;
-					} else {
+				float currentHeight = noiseMap[x, y];
+				for (int i = 0; i < regions.Length; i++)
+				{
+					if (currentHeight >= regions[i].height)
+					{
+						colourMap[y * mapChunkSize + x] = regions[i].colour;
+					}
+					else
+					{
 						break;
 					}
 				}
 			}
 		}
 
-
-		return new MapData (noiseMap, colourMap);
+		return new MapData(noiseMap, colourMap);
 	}
 
-	void OnValidate() {
-		if (lacunarity < 1) {
+	void OnValidate()
+	{
+		if (lacunarity < 1)
+		{
 			lacunarity = 1;
 		}
-		if (octaves < 0) {
+		if (octaves < 0)
+		{
 			octaves = 0;
 		}
 
-		falloffMap = FalloffGenerator.GenerateFalloffMap (mapChunkSize);
+		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
 	}
 
-	struct MapThreadInfo<T> {
+	struct MapThreadInfo<T>
+	{
 		public readonly Action<T> callback;
 		public readonly T parameter;
 
-		public MapThreadInfo (Action<T> callback, T parameter)
+		public MapThreadInfo(Action<T> callback, T parameter)
 		{
 			this.callback = callback;
 			this.parameter = parameter;
@@ -150,22 +222,134 @@ public class MapGenerator : MonoBehaviour {
 
 	}
 
+	// Add this method to calculate terrain normals
+	private Vector3[,] CalculateTerrainNormals(float[,] heightMap)
+	{
+		int width = heightMap.GetLength(0);
+		int height = heightMap.GetLength(1);
+		Vector3[,] normals = new Vector3[width, height];
+
+		for (int y = 1; y < height - 1; y++)
+		{
+			for (int x = 1; x < width - 1; x++)
+			{
+				float heightL = heightMap[x - 1, y];
+				float heightR = heightMap[x + 1, y];
+				float heightU = heightMap[x, y + 1];
+				float heightD = heightMap[x, y - 1];
+
+				Vector3 tangentX = new Vector3(2, (heightR - heightL) * meshHeightMultiplier, 0).normalized;
+				Vector3 tangentY = new Vector3(0, (heightU - heightD) * meshHeightMultiplier, 2).normalized;
+
+				normals[x, y] = Vector3.Cross(tangentX, tangentY).normalized;
+			}
+		}
+		return normals;
+	}
+
+	// Add this method for vegetation placement
+	public void PlaceVegetation(MapData mapData, Vector2 centre)
+	{
+		// Clear existing vegetation
+		if (vegetationParent != null)
+		{
+			DestroyImmediate(vegetationParent.gameObject);
+		}
+		vegetationParent = new GameObject("Vegetation_" + centre.ToString()).transform;
+		vegetationParent.parent = transform;
+
+		float[,] heightMap = mapData.heightMap;
+		Vector3[,] normals = CalculateTerrainNormals(heightMap);
+
+		// Calculate world position offset
+		Vector3 positionOffset = new Vector3(centre.x, 0, centre.y);
+
+		for (int y = 1; y < (mapChunkSize - 1); y++)
+		{
+			for (int x = 1; x < (mapChunkSize - 1); x++)
+			{
+				float worldHeight = heightMap[x, y] * meshHeightMultiplier;
+				float evaluatedHeight = (worldHeight) + 100;
+				Vector3 normal = normals[x, y];
+				float slope = Vector3.Angle(normal, Vector3.up);
+
+				foreach (VegetationRule rule in vegetationRules)
+				{
+					if (worldHeight >= rule.minHeight &&
+							worldHeight <= rule.maxHeight &&
+							slope <= rule.maxSlopeAngle)
+					{
+						if (UnityEngine.Random.value < rule.density * placementThreshold)
+						{
+							// Calculate world position
+							Vector3 position = new Vector3(x * 50, evaluatedHeight, y * 50) + positionOffset;
+
+							// Add random offset within the cell
+							position += new Vector3(
+									(UnityEngine.Random.value - 0.5f),
+									0,
+									(UnityEngine.Random.value - 0.5f)
+							);
+
+							GameObject vegetation = Instantiate(
+									rule.prefab,
+									position,
+									Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0),
+									vegetationParent
+							);
+
+							float scale = UnityEngine.Random.Range(rule.minScale, rule.maxScale);
+							vegetation.transform.localScale *= scale;
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
 
 [System.Serializable]
-public struct TerrainType {
+public struct TerrainType
+{
 	public string name;
 	public float height;
 	public Color colour;
 }
 
-public struct MapData {
+[System.Serializable]
+public class VegetationRule
+{
+	public string name;
+	public GameObject prefab;
+	public float minHeight;
+	public float maxHeight;
+	public float density; // 0-1
+	public float maxSlopeAngle = 45f;
+	public float minScale = 0.8f;
+	public float maxScale = 1.2f;
+}
+
+public struct MapData
+{
 	public readonly float[,] heightMap;
 	public readonly Color[] colourMap;
 
-	public MapData (float[,] heightMap, Color[] colourMap)
+	public MapData(float[,] heightMap, Color[] colourMap)
 	{
 		this.heightMap = heightMap;
 		this.colourMap = colourMap;
+	}
+}
+
+public struct VegetationPlacementRequest
+{
+	public MapData mapData;
+	public Vector2 centre;
+
+	public VegetationPlacementRequest(MapData mapData, Vector2 centre)
+	{
+		this.mapData = mapData;
+		this.centre = centre;
 	}
 }
